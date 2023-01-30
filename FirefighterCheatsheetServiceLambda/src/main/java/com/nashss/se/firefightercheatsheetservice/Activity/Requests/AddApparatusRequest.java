3b60ae20cc4a1de10159adb9105dfdb7d3d62f30
Alex Murillo <alex.d.murillo@protonmail.com>
2023-01-30 15:45:29 -0600

    Start to build out AddApparatusLambda

---
+++ FirefighterCheatsheetServiceLambda/src/main/java/com/nashss/se/firefightercheatsheetservice/Activity/Requests/AddApparatusRequest.java
@@ -0,0 +1,4 @@
+package com.nashss.se.firefightercheatsheetservice.Activity.Requests;
+
+public class AddApparatusRequest {
+}
--- FirefighterCheatsheetServiceLambda/src/main/java/com/nashss/se/firefightercheatsheetservice/Dependency/ServiceComponent.java
+++ FirefighterCheatsheetServiceLambda/src/main/java/com/nashss/se/firefightercheatsheetservice/Dependency/ServiceComponent.java
@@ -20,5 +20,6 @@ public interface ServiceComponent {
      */
     GetApparatusActivity provideGetApparatusActivity();
 
+    AddApparatusActivity provideAddApparatusActivity();
 
 }
