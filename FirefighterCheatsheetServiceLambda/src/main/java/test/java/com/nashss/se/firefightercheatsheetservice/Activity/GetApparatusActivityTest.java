57a63541f0cf9b909f19d3b21dae08a8a5acdcde
Alex Murillo <alex.d.murillo@protonmail.com>
2023-01-31 10:47:25 -0600

    Add GetApparatusActivityTest

---
+++ FirefighterCheatsheetServiceLambda/src/main/java/test/java/com/nashss/se/firefightercheatsheetservice/Activity/GetApparatusActivityTest.java
@@ -0,0 +1,10 @@
+package test.java.com.nashss.se.firefightercheatsheetservice.Activity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetApparatusActivityTest {
+
+    @org.junit.jupiter.api.Test
+    void handleRequest() {
+    }
+}
