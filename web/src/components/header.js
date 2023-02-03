a72d98933ba229850f9710a05c4979fa37ea7bb0
Alex Murillo <alex.d.murillo@protonmail.com>
2023-02-03 16:33:57 -0600

    The delete button is finally working

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -90,57 +90,72 @@ export default class Header extends BindingClass {
     }
 
     async displayApparatus() {
+        document.getElementById('theDisplayArea').innerHTML = "";
         const apparatusList = await this.client.getApparatus();     //may want to set apparatusList in the datastore later. dunno.
         if (apparatusList.length == 0) {
             document.getElementById('theDisplayArea').innerHTML = "No apparatus exist for this account. Add your apparatus below."
         }
 
+
+
+        var currentLocation = "currentLocation";
+        var currentLocations = [];
+
         for (var i=0; i < apparatusList.length; i++) {
+            
+            let iString = i.toString();
+            currentLocation += iString;
+            console.log("currentLocation: " + currentLocation);
+
             var currentApparatus = apparatusList[i];
             if (currentApparatus.fireDept != null) {
                 var fireDept = currentApparatus.fireDept;
-                var currentApparatus = currentApparatus.apparatusTypeAndNumber;
+                var apparatusTypeAndNumber = currentApparatus.apparatusTypeAndNumber;
 
-                document.getElementById('theDisplayArea').innerHTML += 
-                "<br><li>" + fireDept + 
+                var apparatusInfo = 
+                "<li>" + fireDept + 
                 " " + 
-                currentApparatus + 
+                apparatusTypeAndNumber + 
                 "<span>" +
-                (document.getElementById('deleteButton').innerHTML += " X") +
-                "</span>" +
-                "<div class='editHoses'>" +
-                (document.getElementById('editHoses').innerHTML += " Edit Hoses for " + fireDept + " " + currentApparatus) +
-                " </div></li> ";
-
-                this.createDeleteApparatusButton(currentApparatus);
-
+                    `<div class='delButton' id='${currentLocation}'>X</div>` +
+                "</span><div class='editHoses'>" +
+                `Edit Hoses for ${fireDept} ${apparatusTypeAndNumber}` + "</div></li>";
+                document.getElementById('theDisplayArea').innerHTML += apparatusInfo;
+                // console.log("appratusinfo: " + apparatusInfo);
+                currentLocations.push(currentLocation);
+                // const newHeader = new Header();   //nope
+                currentLocation = "currentLocation";   //reset variable for the next loop
             }
         }
-  
+
+        for (var i=0; i < apparatusList.length; i++) {
+            var currentLocation = currentLocations[i];
+            var apparatusTypeAndNumber = apparatusList[i].apparatusTypeAndNumber;
+
+            this.createDeleteApparatusButton(currentLocation, apparatusTypeAndNumber);
+        }
     }
 
-    async createDeleteApparatusButton(currentApparatus) {
-        const button = document.getElementById('deleteButton');
+    createDeleteApparatusButton(currentLocation, apparatusTypeAndNumber) {
+        console.log("Location: " + currentLocation + " and Apparatus: " + apparatusTypeAndNumber);
+        const button = document.getElementById(currentLocation);
+        console.log("button: ", button);
         button.classList.add('button');
-        button.href = '#';
-        button.innerText = 'X';
+        button.classList.add(currentLocation);
+        // button.href = '#';
+        // button.innerText = 'X';
 
         button.addEventListener('click', async () => {
-            this.client.deleteApparatus(currentApparatus);
-            
-            this.displayApparatus();
-        })
-
+            if (confirm("Click OK to delete this apparatus.") == true) {
+            await this.client.deleteApparatus(apparatusTypeAndNumber);
+            // window.location.reload();
+            await this.displayApparatus();
+            }
+        });
         return button;
-
-        // const delButton = document.getElementById('deleteButton');   //get the location where the action should happen
-        // delButton.addEventListener('click', deleteThenRefresh(currentApparatus));  //with a click, run the client's delete apparatus function, passing in the relevant apparatus
     }
 
-    async deleteThenRefresh(currentApparatus) {
-        await this.client.deleteApparatus(currentApparatus)
-        this.displayApparatus();
-    }
+
 
     async displayAddApparatusMenu() {
         (document.getElementById('addApparatusForm').innerHTML += "<div class='addApp'><form>" +
@@ -151,7 +166,7 @@ export default class Header extends BindingClass {
             "<input type='submit' value='Add Apparatus'></div>"
         );
 
-        this.readyAddApparatus();
+        // this.readyAddApparatus();   //undo this when ready
     }
 
     async readyAddApparatus() {
