4051ca9785554e595d7ea88f1d161bfe01c291e7
Alex Murillo <alex.d.murillo@protonmail.com>
2023-02-18 15:01:35 -0600

    Save point

--- web/src/pages/dispApparatus.js
+++ web/src/pages/dispApparatus.js
@@ -36,7 +36,7 @@ class DispApparatus extends BindingClass {
         // Create a enw datastore with an initial "empty" state.
         this.dataStore = new DataStore(EMPTY_DATASTORE_STATE);
         this.header = new Header(this.dataStore);
-        this.dataStore.addChangeListener(this.displaySearchResults);
+        // this.dataStore.addChangeListener(this.displaySearchResults);
     }
 
     /**
@@ -57,77 +57,77 @@ class DispApparatus extends BindingClass {
      * then updates the datastore with the criteria and results.
      * @param evt The "event" object representing the user-initiated event that triggered this method.
      */
-    async search(evt) {
-        // Prevent submitting the from from reloading the page.
-        evt.preventDefault();
+    // async search(evt) {
+    //     // Prevent submitting the from from reloading the page.
+    //     evt.preventDefault();
 
-        const searchCriteria = document.getElementById('search-criteria').value;
-        const previousSearchCriteria = this.dataStore.get(SEARCH_CRITERIA_KEY);
+    //     const searchCriteria = document.getElementById('search-criteria').value;
+    //     const previousSearchCriteria = this.dataStore.get(SEARCH_CRITERIA_KEY);
 
-        // If the user didn't change the search criteria, do nothing
-        if (previousSearchCriteria === searchCriteria) {
-            return;
-        }
+    //     // If the user didn't change the search criteria, do nothing
+    //     if (previousSearchCriteria === searchCriteria) {
+    //         return;
+    //     }
 
-        if (searchCriteria) {
-            const results = await this.client.search(searchCriteria);
+    //     if (searchCriteria) {
+    //         const results = await this.client.search(searchCriteria);
 
-            this.dataStore.setState({
-                [SEARCH_CRITERIA_KEY]: searchCriteria,
-                [SEARCH_RESULTS_KEY]: results,
-            });
-        } else {
-            this.dataStore.setState(EMPTY_DATASTORE_STATE);
-        }
-    }
+    //         this.dataStore.setState({
+    //             [SEARCH_CRITERIA_KEY]: searchCriteria,
+    //             [SEARCH_RESULTS_KEY]: results,
+    //         });
+    //     } else {
+    //         this.dataStore.setState(EMPTY_DATASTORE_STATE);
+    //     }
+    // }
 
     /**
      * Pulls search results from the datastore and displays them on the html page.
      */
-    displaySearchResults() {
-        const searchCriteria = this.dataStore.get(SEARCH_CRITERIA_KEY);
-        const searchResults = this.dataStore.get(SEARCH_RESULTS_KEY);
+    // displaySearchResults() {
+    //     const searchCriteria = this.dataStore.get(SEARCH_CRITERIA_KEY);
+    //     const searchResults = this.dataStore.get(SEARCH_RESULTS_KEY);
 
-        const searchResultsContainer = document.getElementById('userInterfaceArea');
-        const searchCriteriaDisplay = document.getElementById('search-criteria-display');  //search-criteria-display is now theDisplayArea
-        const searchResultsDisplay = document.getElementById('search-results-display');
+    //     const searchResultsContainer = document.getElementById('userInterfaceArea');
+    //     const searchCriteriaDisplay = document.getElementById('search-criteria-display');  //search-criteria-display is now theDisplayArea
+    //     const searchResultsDisplay = document.getElementById('search-results-display');
 
-        if (searchCriteria === '') {
-            searchResultsContainer.classList.add('hidden');
-            searchCriteriaDisplay.innerHTML = '';
-            searchResultsDisplay.innerHTML = '';
-        } else {
-            searchResultsContainer.classList.remove('hidden');
-            searchCriteriaDisplay.innerHTML = `"${searchCriteria}"`;
-            searchResultsDisplay.innerHTML = this.getHTMLForSearchResults(searchResults);
-        }
-    }
+    //     if (searchCriteria === '') {
+    //         searchResultsContainer.classList.add('hidden');
+    //         searchCriteriaDisplay.innerHTML = '';
+    //         searchResultsDisplay.innerHTML = '';
+    //     } else {
+    //         searchResultsContainer.classList.remove('hidden');
+    //         searchCriteriaDisplay.innerHTML = `"${searchCriteria}"`;
+    //         searchResultsDisplay.innerHTML = this.getHTMLForSearchResults(searchResults);
+    //     }
+    // }
 
     /**
      * Create appropriate HTML for displaying searchResults on the page.
      * @param searchResults An array of playlists objects to be displayed on the page.
      * @returns A string of HTML suitable for being dropped on the page.
      */
-    getHTMLForSearchResults(searchResults) {
-        if (searchResults.length === 0) {
-            return '<h4>No results found</h4>';
-        }
+    // getHTMLForSearchResults(searchResults) {
+    //     if (searchResults.length === 0) {
+    //         return '<h4>No results found</h4>';
+    //     }
 
-        let html = '<table><tr><th>Name</th><th>Song Count</th><th>Tags</th></tr>';
-        for (const res of searchResults) {
-            html += `
-            <tr>
-                <td>
-                    <a href="playlist.html?id=${res.id}">${res.name}</a>
-                </td>
-                <td>${res.songCount}</td>
-                <td>${res.tags?.join(', ')}</td>
-            </tr>`;
-        }
-        html += '</table>';
+    //     let html = '<table><tr><th>Name</th><th>Song Count</th><th>Tags</th></tr>';
+    //     for (const res of searchResults) {
+    //         html += `
+    //         <tr>
+    //             <td>
+    //                 <a href="playlist.html?id=${res.id}">${res.name}</a>
+    //             </td>
+    //             <td>${res.songCount}</td>
+    //             <td>${res.tags?.join(', ')}</td>
+    //         </tr>`;
+    //     }
+    //     html += '</table>';
 
-        return html;
-    }
+    //     return html;
+    // }
 
 }
 
